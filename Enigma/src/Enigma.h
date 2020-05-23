b3b0ac90233918be3d2bc3b012aa3781c425506d
TL044CN <gunnisan2@web.de>
2020-05-23 21:56:41 +0200

    added Engine native Keycodes and Mousecodes

--- Enigma/src/Enigma.h
+++ Enigma/src/Enigma.h
@@ -3,8 +3,10 @@
 //for use in Enigma Applications
 
 #include "Enigma/Application.h"
+#include "Enigma/Input.h"
 #include "Enigma/Layer.h"
 #include "Enigma/Log.h"
+
 #include "Enigma/ImGui/ImGuiLayer.h"
 
 #include "Enigma/EntryPoint.h"
