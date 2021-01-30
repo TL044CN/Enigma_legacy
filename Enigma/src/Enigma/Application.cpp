fc0bdbd7e85330751527f79247c72f23c3df1f83
TL044CN <gunnisan2@web.de>
2021-01-31 00:10:57 +0100

    fixed graphics issues caused by RGB picture formats instead of RGBA

--- Enigma/src/Enigma/Application.cpp
+++ Enigma/src/Enigma/Application.cpp
@@ -19,6 +19,8 @@ namespace Enigma {
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(ENGM_BIND_EVENT_FUNCTION(Application::OnEvent));
 
+		Renderer::Init();
+
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
 
--- Enigma/src/Enigma/Renderer/Renderer.h
+++ Enigma/src/Enigma/Renderer/Renderer.h
@@ -8,6 +8,8 @@ namespace Enigma {
 
 	class Renderer {
 	public:
+		static void Init();
+
 		static void BeginScene(OrthographicCamera& camera);
 		static void EndScene();
 
